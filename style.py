b2e49b521a84f08669e86e52a9c531f0a98a2491
Emil Larsson <emil.larsson@eqtpartners.com>
2019-07-09 16:49:09 +0200

    Add script for styling new image

---
+++ style.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+from PIL import Image
+from argparse import ArgumentParser
+
+from networks import TransformerNet
+from train import load_img
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--log-dir", default="logs/style")
+    parser.add_argument("--image-path")
+    parser.add_argument("--output-path")
+    args = parser.parse_args()
+
+    image = load_img(args.image_path)
+
+    transformer = TransformerNet()
+    ckpt = tf.train.Checkpoint(transformer=transformer)
+    ckpt.restore(tf.train.latest_checkpoint(args.log_dir)).expect_partial()
+
+    transformed_image = transformer(image)
+    transformed_image = tf.cast(
+        tf.squeeze(transformed_image), tf.uint8
+    ).numpy()
+
+    img = Image.fromarray(transformed_image, mode="RGB")
+    img.save(args.output_path)
