affee612cf0505b32cb1fb41293c7e403b19ee7b
Emil Larsson <emil.larsson@eqtpartners.com>
2019-07-24 12:46:19 +0200

    Add utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+
+def load_img(path_to_img):
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.cast(img, tf.float32)
+    img = img[tf.newaxis, :]
+    return img
+
+
+def gram_matrix(input_tensor):
+    result = tf.linalg.einsum("bijc,bijd->bcd", input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1] * input_shape[2], tf.float32)
+    return result / (num_locations)
