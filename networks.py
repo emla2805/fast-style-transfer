5bd6a397c5f8312847f76a8672a2ca5a6a20e1ac
Emil Larsson <emil.larsson@eqtpartners.com>
2020-04-09 20:22:09 +0200

    Use InstanceNormalization from tensorflow-addons

--- networks.py
+++ networks.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, ReLU, UpSampling2D
+from tensorflow.keras.applications import vgg16, VGG16
+
+from tensorflow_addons.layers import InstanceNormalization
 
 
 class ReflectionPadding2D(tf.keras.layers.Layer):
@@ -21,35 +24,6 @@ class ReflectionPadding2D(tf.keras.layers.Layer):
             ],
             "REFLECT",
         )
-
-
-class InstanceNormalization(tf.keras.layers.Layer):
-    """Instance Normalization Layer (https://arxiv.org/abs/1607.08022)."""
-
-    def __init__(self, epsilon=1e-5):
-        super(InstanceNormalization, self).__init__()
-        self.epsilon = epsilon
-
-    def build(self, input_shape):
-        self.scale = self.add_weight(
-            name="scale",
-            shape=input_shape[-1:],
-            initializer=tf.random_normal_initializer(0.0, 0.02),
-            trainable=True,
-        )
-
-        self.offset = self.add_weight(
-            name="offset",
-            shape=input_shape[-1:],
-            initializer="zeros",
-            trainable=True,
-        )
-
-    def call(self, x):
-        mean, variance = tf.nn.moments(x, axes=[1, 2], keepdims=True)
-        inv = tf.math.rsqrt(variance + self.epsilon)
-        normalized = (x - mean) * inv
-        return self.scale * normalized + self.offset
 
 
 class ConvLayer(tf.keras.layers.Layer):
